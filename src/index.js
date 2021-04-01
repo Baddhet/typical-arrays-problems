0bec11485f6709bd57001ea4632d1c3708cf139e
Baddhet <Tenjibiu@gmail.com>
2021-04-01 22:07:16 +0300

    init: passed tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let minValue = array[0];
+  if (array == undefined) {
+    return 0;
+  } else {
+    for (let i = 1; i < array.length; i++) {
+      if (minValue > array[i]) {
+        minValue = array[i];
+      }
+    }
+  }
+  return minValue;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let maxValue = array[0];
+  if (array == undefined) {
+    return 0;
+  } else {
+    for (let i = 1; i < array.length; i++) {
+      if (maxValue < array[i]) {
+        maxValue = array[i];
+      }
+    }
+  }
+  return maxValue;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avgValue = 0;
+  if (array == undefined) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      avgValue += array[i];
+      if (array == undefined){
+        return 0;
+      }
+    }
+  }
+  avgValue /= array.length;
+  return avgValue;
 }
